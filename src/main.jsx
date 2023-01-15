e37d376cb1e2532eb90efd4c3cf3eecf86cb036b
ByronXavierCobosMazon <109458836+ByronXavierCobosMazon@users.noreply.github.com>
2023-01-15 01:52:55 -0500

    error de escritura

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import CrudProducts from "./CrudProducts";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CRUDProducts/>    
+        <CrudProducts/>    
     </React.StrictMode>
 );
